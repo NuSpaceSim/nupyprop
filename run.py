edc573604c3fa8143abd4994a90a9b46246736f2
Sameer Patel <49045759+sameer-patel-1@users.noreply.github.com>
2021-05-25 15:45:29 -0600

    Add files via upload
    Added options for neutrino/anti-neutrino

--- run.py
+++ run.py
@@ -21,23 +21,27 @@ import glob
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-e', '--energy', dest='energy_val', nargs='?', const=np.array([1e7, 1e8, 1e9, 1e10, 1e11]), default=np.array([1e7, 1e8, 1e9, 1e10, 1e11]), help='value of incoming neutrino energy; defaults are 10^7-10^11 GeV')
+# parser.add_argument('-e', '--energy', dest='energy_val', nargs='?', const=np.array([1e7, 1e8, 1e9, 1e10]), default=np.array([1e7, 1e8, 1e9, 1e10, 1e11]), help='value of incoming neutrino energy; defaults are 10^7-10^11 GeV')
+# parser.add_argument('-e', '--energy', dest='energy_val', nargs='?', const=np.logspace(7,11,17), default=np.logspace(7,11,17), help='value of incoming neutrino energy; defaults are 10^7-10^11 GeV')
+parser.add_argument('-e', '--energy', dest='energy_val', nargs='?', const=np.array([10**8.75,10**9,10**9.25,10**9.75,10**10,10**10.25,10**10.75,10**11]), default=np.array([10**8.75,10**9,10**9.25,10**9.75,10**10,10**10.25,10**10.75,10**11]), help='value of incoming neutrino energy; defaults are 10^7-10^11 GeV')
 
-parser.add_argument('-a', '--angle', dest='angle_val', nargs='?', const=np.arange(1,41), default=np.arange(1,36), help='value of angle; defaults are 1-35 degrees')
+# parser.add_argument('-a', '--angle', dest='angle_val', nargs='?', const=np.arange(1,41), default=np.arange(1,41), help='value of angle; defaults are 1-40 degrees')
 
-# parser.add_argument('-a', '--angle', dest='angle_val', nargs='?', const=np.arange(1,36), default=np.arange(1,36), help='value of angle; defaults are 1,3,5,7,10,12,15,17,20,25,30,35 degrees')
+parser.add_argument('-a', '--angle', dest='angle_val', nargs='?', const=np.arange(1,36), default=np.arange(1,36), help='value of angle; defaults are 1,3,5,7,10,12,15,17,20,25,30,35 degrees')
 
 # parser.add_argument('-a', '--angle', dest='angle_val', nargs='?', const=np.array([1,2,3,5,7,10,12,15,17,20,25,30,35]), default=np.array([1,2,3,5,7,10,12,15,17,20,25,30,35]), help='value of angle; defaults are 1,3,5,7,10,12,15,17,20,25,30,35 degrees')
 
-parser.add_argument('-i', '--idepth', dest='idepth_val', nargs='?', type=float, const=4, default=4, help='value of water layer depth; default is 4')
+parser.add_argument('-i', '--idepth', dest='idepth_val', nargs='?', type=float, const=4, default=4, help='value of idepth; default is 4')
 
 parser.add_argument('-l', '--lepton', dest='lepton_id', nargs='?', type=str, const='tau', default='tau', help='particle for energy loss and propagation - can be tau or muon; default is tau')
+
+parser.add_argument('-n', '--matter', dest='matter_id', nargs='?', type=str, const='neutrino', default='neutrino', help='type of nuetrino matter - can be neutrino or anti-neutrino; default is neutrino')
 
 parser.add_argument('-t', '--energy_loss', dest='loss_type', nargs='?', type=str, const='stochastic', default='stochastic', help='energy loss type for lepton - can be stochastic or continuous; default is stochastic')
 
 parser.add_argument('-m', '--material', dest='material_id', nargs='?', type=str, const='rock', default='rock', help='material for energy loss - can be rock or water; default is rock')
 
-parser.add_argument('-x', '--xc_model', dest='xc_model_id', nargs='?', type=str, const='cteq18_nlo', default='cteq18_nlo', help='neutrino cross-section model; default is CT18-NLO')
+parser.add_argument('-x', '--xc_model', dest='xc_model_id', nargs='?', type=str, const='ct18nlo', default='ct18nlo', help='neutrino cross-section model; default is ct18nlo')
 
 parser.add_argument('-p', '--pn_model', dest='pn_model_id', nargs='?', type=str, const='allm', default='allm', help='lepton photonuclear energy loss model; default is allm')
 
@@ -61,6 +65,7 @@ if type(angles) is str:
 
 idepth = int(args.idepth_val)
 lepton = str(args.lepton_id)
+nu_type = str(args.matter_id)
 type_loss = str(args.loss_type)
 material = str(args.material_id)
 cross_section_model = str(args.xc_model_id)
@@ -70,7 +75,8 @@ stat = int(args.stat_val)
 
 start_time = time.time()
 
-prob_dict = Main.main(energies, angles, cross_section_model, pn_model, idepth, lepton, fac_nu, stat, type_loss)
+# prob_dict = Main.main(energies, angles, cross_section_model, pn_model, idepth, lepton, fac_nu, stat, type_loss)
+Main.main(energies, angles, nu_type, cross_section_model, pn_model, idepth, lepton, fac_nu, stat, type_loss)
 
 e_out_files = glob.glob("e_out_*") # cleanup of Fortran e_out files
 
