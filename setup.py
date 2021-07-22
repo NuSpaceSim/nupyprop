46d9b6cf273c3fdcb9e4f58c6c8b5086df1395ba
Alex Reustle <areustledev@gmail.com>
2021-07-22 10:31:02 -0400

    Fixes to requirements, ci tools, metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
 
-EXT1 = Extension(name='propagate', sources=['src/nupyprop/propagate.f90'],
-                 extra_f90_compile_args=['-fopenmp'],
-                 extra_link_args=['-fopenmp'])
+EXT1 = Extension(
+    name="propagate",
+    sources=["src/nupyprop/propagate.f90"],
+    extra_f90_compile_args=["-fopenmp"],
+    extra_link_args=["-fopenmp"],
+)
 
-setup(ext_modules=[EXT1],
-    long_description_content_type='text/markdown',
-    long_description='file: README.md',
-        )
+setup(
+    ext_modules=[EXT1],
+    long_description_content_type="text/markdown",
+    long_description="file: README.md",
+)
