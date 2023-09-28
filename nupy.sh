6f8e575a4894e054aae67d60c4bf1e8bf2a0f243
Diksha Garg <dikgarg@ap21.uc.osg-htc.org>
2023-09-28 11:34:10 -0500

    updating

--- nupy.sh
+++ nupy.sh
@@ -8,12 +8,12 @@ tar -xzf nupyprop.tar.gz -C nupyenv
 . nupyenv/bin/activate
 
 ENERGY=$1
-#ANGLE=$2
-STATS=$2
-PN_MODEL=$3
-HTC=$4
-JOB=$5
+ANGLE=$2
+STATS=$3
+PN_MODEL=$4
+HTC=$5
+JOB=$6
 
 #nupyprop -e $ENERGY -a $ANGLE -s $STATS -htc $HTC -job $JOB
-#nupyprop -e $ENERGY -a $ANGLE -s $STATS -p $PN_MODEL -htc $HTC -job $JOB
-nupyprop -e $ENERGY -s $STATS -p $PN_MODEL -htc $HTC -job $JOB
+nupyprop -e $ENERGY -a $ANGLE -s $STATS -p $PN_MODEL -htc $HTC -job $JOB
+#nupyprop -e $ENERGY -s $STATS -p $PN_MODEL -htc $HTC -job $JOB
